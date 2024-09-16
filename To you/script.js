8d94130df86343c3073c2961c6d1015a82887115
Paul-1511 <139778187+Paul-1511@users.noreply.github.com>
2024-09-16 09:01:44 -0600

    Add files via upload

---
+++ To you/script.js
@@ -0,0 +1,27 @@
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
+const question = document.querySelector(".question");
+const gif = document.querySelector(".gif");
+
+
+yesBtn.addEventListener("click", () => {
+  question.innerHTML = "deveritas quieres? 🥺 jejeje te amo tanto";
+  gif.src = "https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExZTgwMXU5OXZneTRjdDg4Z2c5OHVycWk3dzRvYXF4dTV5c2luaXZodCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/qFmdpUKAFZ6rMobzzu/giphy.webp";
+});
+
+
+noBtn.addEventListener("mouseover", () => {
+  const wrapper = document.querySelector(".wrapper");
+  const wrapperRect = wrapper.getBoundingClientRect();
+  const noBtnRect = noBtn.getBoundingClientRect();
+
+
+  const maxX = wrapperRect.width - noBtnRect.width;
+  const maxY = wrapperRect.height - noBtnRect.height;
+
+  const randomX = Math.floor(Math.random() * maxX);
+  const randomY = Math.floor(Math.random() * maxY);
+
+  noBtn.style.left = randomX + "px";
+  noBtn.style.top = randomY + "px";
+});
